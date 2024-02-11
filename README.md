881f357260ea840ea257c775f9a34dcc7c9a0e88
Suspicious Duck <alternatealex23@proton.me>
2024-02-11 16:05:34 -0600

    implemented the rest of the features, refactoring

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 # aternosapi(-rust)
 It's my C++ project aternosapi, but rewritten in Rust. aternosapi is a library meant to control aternos-hosted servers programmatically using Rust. Remember, this **breaks Aternos' TOS for automation**, you can be banned.
-### Current features
+# Current features
 * Log into an existing aternos account
 * List available servers for said account
-### Soon-to-be-added features
 * Start/stop servers
 * Retrieve statuses of servers
 * View players inside of server
 * Query a specific server to access
+# Usage
+You can add this project as a dependency with
+```bash
+cargo add --git "https://github.com/SuspiciousDuck/aternosapi-rust"
+```
+Afterwards, you're free to use it as you please. <br>
+Here's an example executable:
+```rust
+fn main() {
+    let mut aternos = aternosapi::Instance::new();
+    let result = aternos.login("username", "password");
+    if result.is_err() {
+        println!("Error signing in: {}", result.err().unwrap());
+        std::process::exit(1);
+    }
+    aternos.fetch_servers();
+    println!("{} Servers", aternos.servers.len());
+    for server in aternos.servers {
+        println!("Server: {}, id: {}", server.title, server.id);
+    }
+}
+```
--- src/aternos.rs
+++ src/aternos.rs
@@ -2,16 +2,66 @@ use quickjs_rs::{Context, JsValue};
 use rand::prelude::*;
 use scraper::Element;
 
+#[derive(Clone)]
 pub struct Server {
     pub title: String,
-    pub id: String
+    pub id: String,
+    pub ip: Option<String>,
 }
 
 pub struct Instance {
     client: reqwest::blocking::Client,
     cookies: String,
+    session: Option<String>,
     legit_ajax: String,
-    fake_ajax: (String, String, String)
+    fake_ajax: (String, String, String),
+    pub servers: Vec<Server>,
+    pub is_logged_in: bool
+}
+
+impl Server {
+    pub fn start(&self, aternos: &mut Instance) -> Result<(), String> {
+        if !aternos.is_logged_in { return Err("Not logged in".to_string()); }
+        let result = aternos.get_aternos("/ajax/server/start", Some(self.clone()));
+        if result.is_err() { return Err(result.err().unwrap()); }
+        Ok(())
+    }
+
+    pub fn stop(&self, aternos: &mut Instance) -> Result<(), String> {
+        if !aternos.is_logged_in { return Err("Not logged in".to_string()); }
+        let result = aternos.get_aternos("/ajax/server/stop", Some(self.clone()));
+        if result.is_err() { return Err(result.err().unwrap()); }
+        Ok(())
+    }
+
+    pub fn status(&self, aternos: &mut Instance) -> Result<String, String> {
+        if !aternos.is_logged_in { return Err("Not logged in".to_string()); }
+        let rq = aternos.get_aternos("/ajax/server/get-status", Some(self.clone()));
+        if rq.is_err() { return Err(rq.err().unwrap()); }
+        let result = rq.unwrap();
+        let data = result.get("data").unwrap();
+        Ok(data.get("label").unwrap().to_string())
+    }
+
+    pub fn players(&self, aternos: &mut Instance) -> Result<(i64, Vec<serde_json::Value>), String> {
+        if !aternos.is_logged_in { return Err("Not logged in".to_string()); }
+        let rq = aternos.get_aternos("/ajax/server/get-status", Some(self.clone()));
+        if rq.is_err() { return Err(rq.err().unwrap()); }
+        let result = rq.unwrap();
+        let data = result.get("data").unwrap();
+        let players = data.get("playerlist").unwrap().as_array().unwrap();
+        Ok((data.get("players").unwrap().as_i64().unwrap(), players.clone()))
+    }
+
+    pub fn info(&mut self, aternos: &mut Instance) -> Result<(), String> {
+        if !aternos.is_logged_in { return Err("Not logged in".to_string()); }
+        let rq = aternos.get_aternos("/ajax/server/get-status", Some(self.clone()));
+        if rq.is_err() { return Err(rq.err().unwrap()); }
+        let result = rq.unwrap();
+        let data = result.get("data").unwrap();
+        self.ip = Some(format!("{}:{}", data.get("displayAddress").unwrap().as_str().unwrap(), data.get("port").unwrap().as_str().unwrap()));
+        Ok(())
+    }
 }
 
 impl Instance {
@@ -23,8 +73,20 @@ impl Instance {
         let legit_ajax = Self::get_legitajax(&client);
         let (key, value) = Self::get_fakeajax();
         let fake_ajax = format!("{}:{}", key, value);
-        let cookies = Self::generate_cookies(key.clone(), value.clone(), None, None);
-    Instance { client: (client), cookies: (cookies), legit_ajax: (legit_ajax), fake_ajax: (key, value, fake_ajax) }
+        Instance { client: (client), cookies: (String::new()), session: (None),
+            legit_ajax: (legit_ajax), fake_ajax: (key, value, fake_ajax),
+            is_logged_in: (false), servers: Vec::new() }
+    }
+
+    pub fn find_server(&mut self, server: String) -> Result<Server, ()> {
+        for s in &self.servers {
+            if s.title == server { return Ok(s.clone()); }
+        }
+        self.fetch_servers();
+        for s in &self.servers {
+            if s.title == server { return Ok(s.clone()); }
+        }
+        Err(())
     }
 
     fn replace_match(regex: String, text: &String, replace: String) {
@@ -57,6 +119,9 @@ impl Instance {
                     else { full_cookie.as_str() }.to_string();
                 Self::replace_match(re, &self.cookies, replace);
             }
+            if cookie.name() == "ATERNOS_SESSION" {
+                self.session = Some(cookie.value().to_string());
+            }
         }
     }
     
@@ -81,19 +146,20 @@ impl Instance {
         return (Self::base36_encode(rng.gen()), Self::base36_encode(rng.gen()));
     }
     
-    fn build_url(url: &str, legit_ajax: String, fake_ajax: String) -> String {
-        return format!("https://aternos.org{}?TOKEN={}&SEC={}", url, legit_ajax, fake_ajax);
+    fn build_url(&self, url: &str) -> String {
+        return format!("https://aternos.org{}?TOKEN={}&SEC={}", url, self.legit_ajax, self.fake_ajax.2);
     }
     
-    fn generate_cookies(key: String, value: String, session: Option<String>, server: Option<String>) -> String {
-        let mut result= format!("ATERNOS_SEC_{}={};", key, value);
-        if session.is_some() { result = format!("{} ATERNOS_SESSION={};", result, session.unwrap()); }
-        if server.is_some() { result = format!("{} ATERNOS_SERVER={};", result, server.unwrap()); }
-        return result;
+    fn generate_cookies(&mut self, server: Option<Server>) {
+        let mut result= format!("ATERNOS_SEC_{}={};", self.fake_ajax.0, self.fake_ajax.1);
+        if self.session.is_some() { result = format!("{} ATERNOS_SESSION={};", result, self.session.as_ref().unwrap()); }
+        if server.is_some() { result = format!("{} ATERNOS_SERVER={};", result, server.unwrap().id); }
+        self.cookies = result;
     }
     
     pub fn login(&mut self, username: &str, password: &str) -> Result<(), String> {
-        let login_url = Self::build_url("/ajax/account/login", self.legit_ajax.clone(), self.fake_ajax.2.clone());
+        self.generate_cookies(None);
+        let login_url = Self::build_url(&self, "/ajax/account/login");
         let user = username.to_lowercase();
         let pass = md5::compute(password);
         let form_map = reqwest::blocking::multipart::Form::new()
@@ -106,7 +172,8 @@ impl Instance {
         let mut cookies = rq.cookies();
         let result = json.get("success").unwrap().as_bool().unwrap() == true;
         if result {
-            Self::absorb_cookies(self, cookies.by_ref());
+            self.absorb_cookies(cookies.by_ref());
+            self.is_logged_in = true;
             Ok(())
         } else {
             let err = json.get("error").unwrap().to_string();
@@ -114,18 +181,30 @@ impl Instance {
         }
     }
     
-    pub fn get_servers(&self) -> Vec<Server> {
+    pub fn fetch_servers(&mut self) {
         let html = self.client.get("https://aternos.org/servers").header("Cookie", self.cookies.clone()).send().unwrap().text().unwrap();
         let xpath_expr = ".servercard";
         let dom = scraper::Html::parse_document(&html);
         let sel = scraper::Selector::parse(xpath_expr).unwrap();
-        let mut list: Vec<Server> = Vec::new();
         for element in dom.select(&sel) {
             let title = element.attr("title").unwrap();
             let id = element.first_element_child().unwrap().attr("data-id").unwrap();
-            let server = Server{title: title.to_string(), id: id.to_string()};
-            list.push(server);
+            let server = Server{title: title.to_string(), id: id.to_string(), ip: None};
+            let mut exists = false;
+            for s in &self.servers {
+                if s.id == server.id { exists = true; }
+            }
+            if !exists { self.servers.push(server); }
         }
-        return list;
+    }
+    pub fn get_aternos(&mut self, uri: &str, server: Option<Server>) -> Result<serde_json::Map<String, serde_json::Value>, String> {
+        self.generate_cookies(server);
+        let url = self.build_url(uri);
+        let rq = self.client.get(url).header("Cookie", &self.cookies).send().unwrap();
+        let result: serde_json::Map<String, serde_json::Value> = rq.json().unwrap();
+        if !result.get("success").unwrap().as_bool().unwrap() {
+            return Err(result.get("error").unwrap().as_str().unwrap().to_string());
+        }
+        Ok(result)
     }
 }
